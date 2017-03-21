55944cac806b8b20e7910f75dbdb8b9ae97f6979
Leonardo34 <leo_broch@hotmail.com>
2017-03-21 14:57:50 -0300

    fix indentation

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+guests = []
 
 @app.route('/', methods=['GET'])
 def verify():
--- model.py
+++ model.py
@@ -1,20 +1,20 @@
 class Guest:
-	def __init__(self, id):
-		self.id = id
+    def __init__(self, id):
+	    self.id = id
 
-	def set_name(self, name):
-		self.name = name
+    def set_name(self, name):
+	    self.name = name
 
-	def set_last_name(self, last_name):
-		self.last_name = last_name
+    def set_last_name(self, last_name):
+	    self.last_name = last_name
 
-	def set_email(self, email): 
-		self.email = email
+    def set_email(self, email): 
+	    self.email = email
 
-	def set_phone(self, phone):
-		self.phone = phone
+    def set_phone(self, phone):
+	    self.phone = phone
 
-	def set_optin(self, optin):
-		self.optin = optin
+    def set_optin(self, optin):
+	    self.optin = optin
 
 	
